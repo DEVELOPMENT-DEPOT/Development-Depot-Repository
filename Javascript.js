4323f3727fd023d0453e0ffa8375e9613fad906d
mloisking <126358439+mloisking@users.noreply.github.com>
2023-04-16 00:18:17 -0400

    Updated file names of html and css files and updated linkedin logo and link

--- Javascript.js
+++ Javascript.js
@@ -1,17 +1,1 @@
-console.log("Hello World")
-const form = document.querySelector("#contact")
-   const submitButton = document.querySelector("contact")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
-
-   form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
-     fetch(scriptURL, { method: 'POST', body: requestBody})
-       .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
-         })
-       .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false}
+console.log("Hello World!")
